43afc2c52a60da267d152c1f7c9d9b2d217867c5
Na <raja28chowdhury@gmail.com>
2023-10-07 16:40:06 +0200

    added a cpp stack project but implementation is not done yet

---
+++ cpp_stack/main.cpp
@@ -0,0 +1,8 @@
+// Your First C++ Program
+
+#include <iostream>
+#include "stack.h"
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
---
+++ cpp_stack/tests/my_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "../stack.h"
+
+
+TEST(FIST,NAME){
+    EXPECT_EQ(1,1);
+    EXPECT_EQ(2,2);
+}
