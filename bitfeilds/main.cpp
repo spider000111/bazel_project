6e3779f7bba47f5cec521a92b8069f344cf56721
raja chowdhury <raja28chowdhury@gmail.com>
2024-01-16 19:06:42 +0530

    added bitfeilds code

--- bitfeilds/main.cpp
+++ bitfeilds/main.cpp
@@ -4,7 +4,7 @@
 
 struct unpack_struct1 {  //this consume 4 bute
     unsigned int x : 8; //8 is bits here so this is one byte
-    unsigned int y : 8; 
+    int y : 3;          //1st bit here will be sign bit
     //unsigned int z : 3; 
 };
 
@@ -30,7 +30,9 @@ struct unpack_force_alinment {  //this consume 8 bytes
 int main() {
 
     printf("Size of unpack_struct1 is %lu bytes\n",sizeof(struct unpack_struct1));
-
+    struct unpack_struct1 temp ;
+    temp.y = 6;
+    printf("value of unpack_struct1 is %d \n",temp.y );
 
     printf("Size of pack_struct1 is %lu bytes\n",sizeof(struct pack_struct1));
 
